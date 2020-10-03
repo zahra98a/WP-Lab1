9337ea7add1acd79bb5129111b535f8217b8dab0
Zahra Al Bulushi <47219252+zahra98a@users.noreply.github.com>
2020-10-03 11:50:24 +0100

    Add files via upload
    add second route

--- firstApp/index.js
+++ firstApp/index.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 4000
 
 
-app.get('/', (req, res) => res.send('Hello World from Zahra! how are you doing?'))
+//app.get('/', (req, res) => res.send('Hello World from Zahra! how are you doing?'))
+app.get('/bananas', (req, res) => res.send('hello world, this is bananas'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
